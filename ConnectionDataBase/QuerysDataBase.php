c9cda846caa791c3cfc69a8972eff8725e4f1cf1
Laurina Bruges <lbrugues1@gmail.com>
2024-04-11 22:45:38 -0500

    Inicio de sesión

--- ConnectionDataBase/QuerysDataBase.php
+++ ConnectionDataBase/QuerysDataBase.php
@@ -1,5 +1,23 @@
 <?php
+require("../ConnectionDataBase/ConnectionDataBase.php");
+
 class QuerysDataBase
 {
+    function doLogin($var){
+        $conn = new ConnectionDataBase();
+        
+        if(!isset($var["username"])){
+            return false;
+        }
+        if(!isset($var["password"])){
+            return false;
+        }
+        $input_username = $var["username"];
+        $input_password = $var["password"];
+        // Consulta SQL para verificar las credenciales
+        $sql = "SELECT * FROM usuarios WHERE num_identificacion='$input_username' AND contraseña='$input_password'";
+        $result = $conn->Execute($sql);
 
+        return $result->num_rows > 0;
+    }
 }
